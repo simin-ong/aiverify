83da2fa9a9971ce3276e2d14a5b31ab2efc662bb
simin-ong <ongsimin2000@gmail.com>
2024-01-22 16:45:00 +0800

    added some test files for json serializer

--- test-engine-core-modules/src/jsonserializer/test.py
+++ test-engine-core-modules/src/jsonserializer/test.py
@@ -1,10 +1,14 @@
 import json
 from pandas import DataFrame
+import pandas as pd
 
 data_path = "user_defined_files/teststring.json"
+csv_file_path = "user_defined_files/test_drifted.csv"
+json_file_path = "user_defined_files/test_drifted.json"
 
 with open(data_path, "r") as data:
     file = json.load(data)
+print(type(file))
 df = DataFrame.from_dict(file)
 print(df)
 
@@ -13,3 +17,12 @@ if isinstance(df, DataFrame):
     for key in df.keys():
         output_dict[key] = df.dtypes[key].name
 print(output_dict)
+
+df = pd.read_csv(csv_file_path)
+df.to_json(json_file_path, orient='records', indent=2)
+
+with open(json_file_path, "r") as data:
+    file = json.load(data)
+print(type(file))
+new_df = DataFrame.from_dict(file)
+print(new_df)
